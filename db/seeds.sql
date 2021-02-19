6ee384f8e880a01788017494881c99548498c1cb
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-02-18 22:18:19 -0800

    replaced 'cats' with 'burgers'

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burger (name) VALUES ('Two by two');
+INSERT INTO burger (name) VALUES ('Super star with cheese');
+INSERT INTO burger (name) VALUES ('Jalapeno 1/3rd pounder');
+INSERT INTO burger (name) VALUES ('Spicy buffalo chicken');
+INSERT INTO burger (name) VALUES ('McRib');
+INSERT INTO burger (name) VALUES ('Animal-style Double Double with chiles, no pickles');
